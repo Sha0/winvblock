ba4956aacd29c7a5c63c8ebca52e9a2e7f705cce
Shao Miller <Shao.Miller@yrdsb.edu.on.ca>
2009-12-11 11:45:59 -0500

    [headers] Introduce winvblock header for common material
    winvblock.h will help to facilitate code style and abstraction.
    Currently, it provides a means to define and typedef structures in code.

---
+++ src/winvblock.h
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) 2009, Shao Miller <shao.miller@yrdsb.edu.on.ca>.
+ *
+ * This file is part of WinVBlock, derived from WinAoE.
+ * For WinAoE contact information, see http://winaoe.org/
+ *
+ * WinVBlock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * WinVBlock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with WinVBlock.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _winvblock_h
+#  define _winvblock_h
+
+/**
+ * @file
+ *
+ * WinVBlock project common material
+ *
+ */
+
+/* A common way to define a structure */
+#  define winvblock__def_struct( x ) \
+\
+struct _##x;\
+typedef struct _##x x, *x##_ptr;\
+struct _##x
+
+#endif													/* _winvblock_h */
