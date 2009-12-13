2074c5ef64ea242865d53a7a87b5523cdb6e1b47
Shao Miller <Shao.Miller@yrdsb.edu.on.ca>
2009-12-13 01:12:41 -0500

    [cosmetics] indent_add.sh shell script added
    This script indents and does 'git add' to specified files.

---
+++ indent_add.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+indent -bap -bc -bfda -bfde -bl -bli2 -bls -cbi2 -cdb -cdw -nce -ci2 -cli2 -i2 -ip2 -kr -l79 -lp -ncs -nhnl -pcs -pi2 -ppi2 -prs -psl -saf -sai -saw -sc -sob -ts2 $*
+git add $*
