2669130893f1f61ae79a069bbd621bcbbf911808
Shao Miller <Shao.Miller@yrdsb.edu.on.ca>
2009-12-28 22:28:35 -0500

    [cosmetics] Run indent twice due to indent bug
    You might or might not have noticed that 'indent' will take
    a file and change its mind about how to indent it.  Take that
    resulting file and run 'indent' again and it will put the file
    back the way it was.  Thus there is an "even" format and an
    "odd" format.  This is silly.  As a work-around, the indent.sh
    and indent_add.sh scripts will run 'indent' twice on a file.
    
    Yeesh.

--- indent.sh
+++ indent.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Twice because of some indent bug which changes a file back and forth
+indent -bap -bc -bfda -bfde -bl -bli2 -bls -cbi2 -cdb -cdw -nce -ci2 -cli2 -i2 -ip2 -kr -l79 -lp -ncs -nhnl -pcs -pi2 -ppi2 -prs -psl -saf -sai -saw -sc -sob -ts8 $*
 indent -bap -bc -bfda -bfde -bl -bli2 -bls -cbi2 -cdb -cdw -nce -ci2 -cli2 -i2 -ip2 -kr -l79 -lp -ncs -nhnl -pcs -pi2 -ppi2 -prs -psl -saf -sai -saw -sc -sob -ts8 $*
