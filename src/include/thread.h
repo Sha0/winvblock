dc218eb987e3c320681f80930c02f82c3ccae8e1
Shao Miller <Shao.Miller@yrdsb.edu.on.ca>
2011-09-12 21:48:13 -0400

    [thread] Minor cosmetic changes to header
    Also rename the "Lib" folder in the wxDev-C++ project
    tree to "WvLib".

--- src/include/thread.h
+++ src/include/thread.h
@@ -16,45 +16,37 @@
  * You should have received a copy of the GNU General Public License
  * along with WinVBlock.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef WVL_M_LIBTHREAD_H_
-#  define WVL_M_LIBTHREAD_H_
+#ifndef M_THREAD_H_
 
-/**
+/****
  * @file
  *
  * WinVBlock thread library.
  */
 
-/* Forward declarations. */
-struct WVL_THREAD_ITEM;
+/*** Macros */
+#define M_THREAD_H_
 
-/* Types. */
-typedef VOID STDCALL WVL_F_THREAD_ITEM(IN OUT struct WVL_THREAD_ITEM *);
-typedef WVL_F_THREAD_ITEM * WVL_FP_THREAD_ITEM;
-
-typedef struct WVL_THREAD_ITEM {
-    LIST_ENTRY Link;
-    WVL_FP_THREAD_ITEM Func;
-  } WVL_S_THREAD_ITEM, * WVL_SP_THREAD_ITEM;
-
-typedef enum WVL_THREAD_STATE {
+/*** Constants */
+enum E_WVL_THREAD_STATE_ {
     WvlThreadStateNotStarted,
     WvlThreadStateStarting,
     WvlThreadStateStarted,
     WvlThreadStateStopping,
     WvlThreadStateStopped,
     WvlThreadStates
-  } WVL_E_THREAD_STATE, * WVL_EP_THREAD_STATE;
+  };
 
-typedef struct WVL_THREAD {
-    WVL_S_THREAD_ITEM Main;
-    WVL_E_THREAD_STATE State;
-    KSPIN_LOCK Lock;
-    KEVENT Signal;
-    HANDLE Handle;
-    PETHREAD Thread;
-  } WVL_S_THREAD, * WVL_SP_THREAD;
+/*** Object types */
+typedef struct S_WVL_THREAD_ITEM_ WVL_S_THREAD_ITEM, * WVL_SP_THREAD_ITEM;
+typedef enum E_WVL_THREAD_STATE_ WVL_E_THREAD_STATE, * WVL_EP_THREAD_STATE;
+typedef struct S_WVL_THREAD_ WVL_S_THREAD, * WVL_SP_THREAD;
 
+/*** Function types */
+typedef VOID STDCALL WVL_F_THREAD_ITEM(IN OUT WVL_SP_THREAD_ITEM);
+typedef WVL_F_THREAD_ITEM * WVL_FP_THREAD_ITEM;
+
+/*** Function declarations */
 extern WVL_M_LIB NTSTATUS WvlThreadStart(IN OUT WVL_SP_THREAD);
 extern WVL_M_LIB BOOLEAN STDCALL WvlThreadSendStopAndWait(IN WVL_SP_THREAD);
 extern WVL_M_LIB WVL_SP_THREAD WvlThreadGetCurrent(void);
@@ -66,4 +58,19 @@ extern WVL_M_LIB WVL_SP_THREAD_ITEM STDCALL WvlThreadGetItem(IN WVL_SP_THREAD);
 extern WVL_M_LIB VOID STDCALL WvlThreadTest(IN OUT WVL_SP_THREAD_ITEM);
 extern WVL_M_LIB VOID WvlThreadTestMsg(IN PCHAR);
 
-#endif  /* WVL_M_LIBTHREAD_H_ */
+/*** Struct/union definitions */
+struct S_WVL_THREAD_ITEM_ {
+    LIST_ENTRY Link;
+    WVL_FP_THREAD_ITEM Func;
+  };
+
+struct S_WVL_THREAD_ {
+    WVL_S_THREAD_ITEM Main;
+    WVL_E_THREAD_STATE State;
+    KSPIN_LOCK Lock;
+    KEVENT Signal;
+    HANDLE Handle;
+    PETHREAD Thread;
+  };
+
+#endif  /* M_THREAD_H_ */
